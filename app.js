d0d8a6d30f608b74851a0dd19d099c48c24b0152
goodguyerik <56648310+goodguyerik@users.noreply.github.com>
2022-02-03 19:15:01 +0100

    add formular selection, success alert message, mess up code even more

--- app.js
+++ app.js
@@ -9,8 +9,13 @@ app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+let url = process.env.DATABASE_URL;
+if(url == null || url == ""){
+  url = "postgres...";
+}
+
 const pool = new Pool({
-  connectionString: "postgres://hkimycgktprnvb:a0d19a7bc6e693043d21b5239e1e01b931871cc441e4cfabc6611a4222319cc8@ec2-34-246-24-110.eu-west-1.compute.amazonaws.com:5432/db7kbeb6moq53i",
+  connectionString: url,
   ssl: {
     rejectUnauthorized: false
   }
@@ -23,7 +28,7 @@ pool.connect();
 // });
 
 //create tables if they dont exist
-pool.query('CREATE TABLE IF NOT EXISTS formular (id SERIAL PRIMARY KEY,datum DATE,mitarbeiter VARCHAR(50),projektkennung VARCHAR(300),nachgeschautes_erfahrungswissen VARCHAR(5000),gelerntes VARCHAR(5000));CREATE TABLE IF NOT EXISTS arbeitsschritte (name VARCHAR(50) PRIMARY KEY);CREATE TABLE IF NOT EXISTS recherche (name VARCHAR(50) PRIMARY KEY);CREATE TABLE IF NOT EXISTS formular_arbeitsschritte (form_id SERIAL,as_name VARCHAR(50),PRIMARY KEY(form_id, as_name),FOREIGN KEY (form_id) REFERENCES formular(id),FOREIGN KEY (as_name) REFERENCES arbeitsschritte(name));CREATE TABLE IF NOT EXISTS formular_recherche (form_id SERIAL,re_name VARCHAR(50),beschreibung VARCHAR(500),PRIMARY KEY(form_id, re_name),FOREIGN KEY (form_id) REFERENCES formular(id),FOREIGN KEY (re_name) REFERENCES recherche(name));', function(err, res){
+pool.query('CREATE TABLE IF NOT EXISTS formular (id SERIAL PRIMARY KEY,formular VARCHAR(5), datum DATE,mitarbeiter VARCHAR(50),projektkennung VARCHAR(300),nachgeschautes_erfahrungswissen VARCHAR(5000),gelerntes VARCHAR(5000));CREATE TABLE IF NOT EXISTS arbeitsschritte (name VARCHAR(50) PRIMARY KEY);CREATE TABLE IF NOT EXISTS recherche (name VARCHAR(50) PRIMARY KEY);CREATE TABLE IF NOT EXISTS formular_arbeitsschritte (form_id SERIAL,as_name VARCHAR(50),PRIMARY KEY(form_id, as_name),FOREIGN KEY (form_id) REFERENCES formular(id),FOREIGN KEY (as_name) REFERENCES arbeitsschritte(name));CREATE TABLE IF NOT EXISTS formular_recherche (form_id SERIAL,re_name VARCHAR(50),beschreibung VARCHAR(500),PRIMARY KEY(form_id, re_name),FOREIGN KEY (form_id) REFERENCES formular(id),FOREIGN KEY (re_name) REFERENCES recherche(name));', function(err, res){
   if(!err){
     //fill tables with data if these not already exist - arbeitsschritte
     pool.query("INSERT INTO arbeitsschritte VALUES($1), ($2), ($3), ($4), ($5), ($6), ($7), ($8), ($9), ($10), ($11), ($12), ($13), ($14), ($15), ($16), ($17), ($18), ($19), ($20), ($21), ($22), ($23), ($24) ON CONFLICT DO NOTHING;",['makrofoto', 'einbetten_vor_trennen', 'trennen', 'einbetten', 'schleifen', 'polieren', 'endpolieren', 'vibrationspolieren', 'ätzen', 'lichtmikroskopie', 'härtemessung', 'ionenpolitur_csp', 'ionenpolitur_pips', 'fib', 'e-polieren', 'werkstoff', 'herstellungsverfahren', 'wärmebehandlung', 'probenanzahl/versuchsreihen', 'probengeometrie', 'probenvorbereitung', 'versuchsstand_einrichten', 'versuchsdurchführung', 'auswertung/prüfbericht'], function(err, res){
@@ -47,13 +52,19 @@ pool.query('CREATE TABLE IF NOT EXISTS formular (id SERIAL PRIMARY KEY,datum DAT
 // });
 
 app.get("/", function(req, res) {
-  res.render("selection");
+  res.render("selection", {success: false});
+});
+
+app.get("/success", function(req, res){
+  res.render("selection", {success: true});
 });
 
 app.get("/data", function(req, res) {
       var form_result = [];
       var abs_result = [];
       var re_result = [];
+      var arbeitsschritte_result = [];
+      var recherche_result = [];
       pool.query('SELECT * FROM formular', function(form_err, form_outcome) {
         if (form_err) {
           console.log(form_err);
@@ -69,10 +80,77 @@ app.get("/data", function(req, res) {
                   console.log(re_err);
                 } else {
                   re_result = re_outcome.rows;
-                  res.render("data", {
-                    formular: form_result,
-                    abs: abs_result,
-                    re: re_result
+                  pool.query('SELECT * FROM arbeitsschritte', function(arbeitsschritte_err, arbeitsschritte_outcome) {
+                    if(arbeitsschritte_err){
+                      console.log(arbeitsschritte_err);
+                    }else{
+                      arbeitsschritte_result = arbeitsschritte_outcome.rows;
+                      pool.query('SELECT * FROM recherche', function(recherche_err, recherche_outcome) {
+                        if(recherche_err){
+                          console.log(recherche_err);
+                        }else{
+                          recherche_result = recherche_outcome.rows;
+                          res.render("data", {
+                            formular: form_result,
+                            abs: abs_result,
+                            re: re_result,
+                            arbeit: arbeitsschritte_result,
+                            recherche: recherche_result
+                          });
+                        }
+                      });
+                    }
+                  });
+                }
+              });
+            }
+          });
+        }
+      });
+});
+
+app.get("/data/:x", function(req, res) {
+      const x = req.params.x;
+      var form_result = [];
+      var abs_result = [];
+      var re_result = [];
+      var arbeitsschritte_result = [];
+      var recherche_result = [];
+      pool.query('SELECT * FROM formular WHERE formular = $1',[x], function(form_err, form_outcome) {
+        if (form_err) {
+          console.log(form_err);
+        } else {
+          form_result = form_outcome.rows;
+          pool.query('SELECT * FROM formular_arbeitsschritte', function(abs_err, abs_outcome) {
+            if (abs_err) {
+              console.log(abs_err);
+            } else {
+              abs_result = abs_outcome.rows;
+              pool.query('SELECT * FROM formular_recherche', function(re_err, re_outcome) {
+                if (re_err) {
+                  console.log(re_err);
+                } else {
+                  re_result = re_outcome.rows;
+                  pool.query('SELECT * FROM arbeitsschritte', function(arbeitsschritte_err, arbeitsschritte_outcome) {
+                    if(arbeitsschritte_err){
+                      console.log(arbeitsschritte_err);
+                    }else{
+                      arbeitsschritte_result = arbeitsschritte_outcome.rows;
+                      pool.query('SELECT * FROM recherche', function(recherche_err, recherche_outcome) {
+                        if(recherche_err){
+                          console.log(recherche_err);
+                        }else{
+                          recherche_result = recherche_outcome.rows;
+                          res.render("data", {
+                            formular: form_result,
+                            abs: abs_result,
+                            re: re_result,
+                            arbeit: arbeitsschritte_result,
+                            recherche: recherche_result
+                          });
+                        }
+                      });
+                    }
                   });
                 }
               });
@@ -85,17 +163,18 @@ app.get("/data", function(req, res) {
 app.get("/:form", function(req, res){
   const form = req.params.form;
   res.render(form + ".ejs");
-})
+});
 
 app.post("/", function(req, res){
   const body = req.body;
+  const formular = req.body.titel;
   const datum = req.body.datum;
   const mitarbeiter = req.body.mitarbeiter;
   const projekt = req.body.projektkennung;
   const erfahrung = req.body.erfahrungswissen;
   const gelerntes = req.body.gelerntes;
 
-  pool.query('INSERT INTO formular VALUES(DEFAULT, $1, $2, $3, $4, $5) RETURNING id;', [datum, mitarbeiter, projekt, erfahrung, gelerntes] , function(err, res){
+  pool.query('INSERT INTO formular VALUES(DEFAULT, $1, $2, $3, $4, $5, $6) RETURNING id;', [formular, datum, mitarbeiter, projekt, erfahrung, gelerntes] , function(err, res){
     if(err){
       console.log(err);
     }else{
@@ -122,7 +201,7 @@ app.post("/", function(req, res){
     }
   });
   console.log("Das Formular wurde gespeichert!");
-  res.redirect("/");
+  res.redirect("/success");
 });
 
 
